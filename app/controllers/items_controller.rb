ef66d8e18673506dd7120efa4bb87c8435da6cfd
Abid Mahamud <abidmahamood88@gmail.com>
2024-05-29 08:53:36 +0600

    Non-authenticated User's Item details view added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, expect: [:index, :show]
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :set_collection
     before_action :set_item, only: [:show, :edit, :update, :destroy]
     before_action :authorize_user!, only: [:edit, :update, :destroy]
