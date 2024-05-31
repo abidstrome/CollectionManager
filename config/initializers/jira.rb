6437bc11eff1cdfa9b0a5972da4280822be11ceb
Abid Mahamud <abidmahamood88@gmail.com>
2024-05-31 12:14:38 +0600

    Updated search funtionality & View

---
+++ config/initializers/jira.rb
@@ -0,0 +1,8 @@
+JIRA_CLIENT = JIRA::Client.new(
+    site: 'https://tressurex.atlassian.net',
+    context_path: '',
+    auth_type: :basic,
+    username: 'abidmahamood88@gmail.com',
+    password: 'ATATT3xFfGF02x-nikJeAy7uNhGc8-KnIFIyEORKvZEPlJJ0ZhChibo2Yi8AdZFrJlDrDECBXIZmMng1FNJsCtplKNlQaNSXTx3_BV5JFEFxTriEttNNeMYXRbZO4jvHwqyqwNHgwaQP8L1O4sZEhYrGUbKpP663ENFIWf7EFEpVzTlzX6lOehY=701BE9E4'
+
+)
