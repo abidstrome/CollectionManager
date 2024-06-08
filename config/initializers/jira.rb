8722ae78736f9df007aded22567470e8b7a99b16
Abid Mahamud <abidmahamood88@gmail.com>
2024-06-09 04:09:06 +0600

    JIRA API TOKEN UPDATED

--- config/initializers/jira.rb
+++ config/initializers/jira.rb
@@ -2,7 +2,7 @@ JIRA_CLIENT = JIRA::Client.new(
     site: 'https://tressurex.atlassian.net',
     context_path: '',
     auth_type: :basic,
-    username: ENV['JIRA_USERNAME'],
-    password: ENV['JIRA_API_TOKEN']
+    username: 'abidmahamood88@gmail.com',
+    password: 'ATATT3xFfGF02x-nikJeAy7uNhGc8-KnIFIyEORKvZEPlJJ0ZhChibo2Yi8AdZFrJlDrDECBXIZmMng1FNJsCtplKNlQaNSXTx3_BV5JFEFxTriEttNNeMYXRbZO4jvHwqyqwNHgwaQP8L1O4sZEhYrGUbKpP663ENFIWf7EFEpVzTlzX6lOehY=701BE9E4'
 
 )
