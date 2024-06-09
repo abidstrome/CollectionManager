faeac957cf846d79c3702f950e876a5e6bddd397
Abid Mahamud <abidmahamood88@gmail.com>
2024-06-10 03:00:02 +0600

    Secret removed

--- config/initializers/jira.rb
+++ config/initializers/jira.rb
@@ -2,7 +2,6 @@ JIRA_CLIENT = JIRA::Client.new(
     site: 'https://tressurex.atlassian.net',
     context_path: '',
     auth_type: :basic,
-    username: 'abidmahamood88@gmail.com',
-    password: 'ATATT3xFfGF03v5GPCFmERDbIqsdZZbZ9SND6HHYo52JFOnXtm1-x3UTmrJmDUTyGkeBxWCrE6fCGw369aXGx15CtGiUF8jGSQOYuKFV6VxeG-rJA6lse3lw1zl99HnMzJxjkQYUNNTpx0yPAfKLfHmYUyeUoqcMz6mYtKNHzpX44DY1BGcAmZQ=6F8349C8'
-
+    username: ENV['JIRA_USERNAME'],
+    password: ENV['JIRA_API_TOKEN']
 )
