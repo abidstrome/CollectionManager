49baa14c5607c41dc6d5c79e43cca74fd08e9823
Abid Mahamud <abidmahamood88@gmail.com>
2024-06-11 12:28:01 +0600

    Api update

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,65 +1,65 @@
 class Api::V1::CollectionsController < ApplicationController
-
     include Pundit
     before_action :authenticate_with_token!
     before_action :authorize_collection, only: [:update, :destroy]
-
+  
     def index
-        user = user.find_by(api_token: request.headers['Authorization'])
-        if user
-            collections = user.collections.select(:id, :name, :description, :category_id, :user_id)
-            render json: collections
-        else
-            render json: {error: 'Unauthorized'}, status: :unauthorized
-        end
+      user = User.find_by(api_token: request.headers['Authorization'])
+      if user
+        collections = user.collections.select(:id, :name, :description, :category_id, :user_id).as_json
+        ccollections.each do |collection|
+            collection['item_count'] = Collection.find(collection['id']).items.count
+          end
+        render json: collections
+      else
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
     end
-
+  
     def create
-        user = User.find_by(api_token: request.headers['Authorization'])
-        if user
-            collection = user.collections.build(collection_params)
-            if collection.save
-                render json: collection, status: :created
-            else
-                render json: collection.errors, staus: :unprocessable_entity
-            end
+      user = User.find_by(api_token: request.headers['Authorization'])
+      if user
+        collection = user.collections.build(collection_params)
+        if collection.save
+          render json: collection, status: :created
         else
-            render json: {error: 'Unauthorized'}, status: :unauthorized
+          render json: collection.errors, status: :unprocessable_entity
         end
+      else
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
     end
-
+  
     def update
-        if @collection.update(collection_params)
-            render json: @collection
-        else
-            render json: @collection.errors, status: :unprocessable_entity
-        end
+      if @collection.update(collection_params)
+        render json: @collection
+      else
+        render json: @collection.errors, status: :unprocessable_entity
+      end
     end
-
+  
     def destroy
-        @collection.destroy
-        head :no_content
+      @collection.destroy
+      head :no_content
     end
-
-
+  
     private
-
+  
     def set_collection
-        @collection = Collection.find(params[:id])
+      @collection = Collection.find(params[:id])
     end
-    
+  
     def authenticate_with_token!
-        api_token = request.headers['Authorization']
-        head :unauthorized unless User.exists?(api_token: api_token)
+      api_token = request.headers['Authorization']
+      head :unauthorized unless User.exists?(api_token: api_token)
     end
-
+  
     def collection_params
-        params.require(:collection).permit(:name, :description, :category_id)
+      params.require(:collection).permit(:name, :description, :category_id)
     end
-
+  
     def authorize_collection
-        authorize @collection
+      authorize @collection
     end
-
-
-end
+  end
+  
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -11,6 +11,9 @@ class Collection < ApplicationRecord
                     tsearch: {prefix: true}
                   }
                 
+  def as_json(options = {})
+    super(options.merge(include: { items: { only: [:id] } })).merge(item_count: items.count)
+  end
 
   def any_item
     items.first
